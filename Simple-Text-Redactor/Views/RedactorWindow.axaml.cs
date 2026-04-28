4f92879ee7c149214b52f4d6a82ab5ef5845f6d2
ArGul <argul.mail@gmail.com>
2026-04-28 20:18:10 +0300

    Added DataContext for RedactorWindow.

--- Simple-Text-Redactor/Views/RedactorWindow.axaml.cs
+++ Simple-Text-Redactor/Views/RedactorWindow.axaml.cs
@@ -1,6 +1,7 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Simple_Text_Redactor.ViewModels;
 
 namespace Simple_Text_Redactor.Views;
 
@@ -9,5 +10,6 @@ public partial class RedactorWindow : Window
     public RedactorWindow()
     {
         InitializeComponent();
+        DataContext = new RedactorViewModel();
     }
 }
